469620831367e850ae3febadcb3ab6775e7fce06
Junxiaoniao <junxiaoniao@gmail.com>
2022-03-02 10:29:46 +0800

    JAVA

--- src/main/java/com/junxiaoniao/book/config/MyMVCConfig.java
+++ src/main/java/com/junxiaoniao/book/config/MyMVCConfig.java
@@ -8,13 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyMVCConfig implements WebMvcConfigurer {
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("upload");
-        registry.addViewController("/download").setViewName("download");
-
-    }
-
-    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/picture/**").addResourceLocations("file:E://fileUpload/image/");
     }
