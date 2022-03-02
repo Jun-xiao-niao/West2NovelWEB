49ce8478b6b80eb4a05343eef48e62b6feffa808
Junxiaoniao <junxiaoniao@gmail.com>
2022-03-02 10:29:14 +0800

    JAVA

---
+++ src/test/java/com/junxiaoniao/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junxiaoniao.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+    @Test
+    void Never_Gonna_Give_You_Up() {
+    }
+
+}
